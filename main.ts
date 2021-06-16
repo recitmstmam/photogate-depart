74898c582628331dec89476174da6b3dcf0507ff
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-06-16 15:21:10 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.lightLevel()))
+})
 radio.setGroup(1)
 let t0 = 0
 basic.showLeds(`
@@ -10,7 +13,7 @@ basic.showLeds(`
 pins.analogWritePin(AnalogPin.P0, 1023)
 basic.forever(function () {
     // Modifier la valeur de l'intensité lumineuse en fonction  de vos tests.
-    if (input.lightLevel() < 120) {
+    if (input.lightLevel() < 10) {
         radio.sendNumber(0)
         basic.showIcon(IconNames.Happy)
         radio.setGroup(40)
